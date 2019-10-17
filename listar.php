7b470628ac36bc72a14a0b25eb14d2de9f12fbf6
leonardo7422 <leo_canceriano@hotmail.com>
2019-10-17 03:10:11 -0300

    Correções na pagina listar

--- listar.php
+++ listar.php
@@ -7,21 +7,14 @@
 	include("classeControllerBD.php");
 	
 	
-	$colunas = array(   "id_fornecedor as ID",
-						"Nome_Departamento AS 'Nome do Departamento'",
-						"Endereco as 'Endereço'",
-						"CEP",
-						"Cidade",
-						"Estado",
-						"Nome_pais as 'País'",
-						"Nome_regiao as 'Região'");
+	$colunas = array();
 	
 	$t[0][0] = "fornecedor";
-	$t[0][1] = "localizacao";
-	$t[1][0] = "localizacao";
-	$t[1][1] = "pais";
-	$t[2][0] = "pais";
-	$t[2][1] = "regiao";
+	$t[0][1] = "compra";
+	$t[1][0] = "loja";
+	$t[1][1] = "estado";
+	$t[2][0] = "produto";
+	$t[2][1] = "cidade";
 	
 	$c = new ControllerBD($conexao);
 	
